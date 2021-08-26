6218a242dd8fb97dd6466ba447b4a40886be8259
George Alusa <40027452+georgealusa@users.noreply.github.com>
2021-08-26 13:38:47 +0300

    Update testComment.py

--- test/testComment.py
+++ test/testComment.py
@@ -4,7 +4,7 @@ import unittest
 
 class CommentModelTest(unittest.TestCase):
     def setUp(self):
-        self.user_Michael = User(username = 'Michael',password = 'banana', email = 'michael.m.njoroge254@gmail.com')
+        self.user_george = User(username = 'george',password = 'banana', email = 'georgealusa254@gmail.com')
         self.new_pitch = Pitch(id=1,pitch_title='Test',pitch_comment='This is a test pitch',category="interview",user = self.user_Dorcas,likes=0,dislikes=0)
         self.new_comment = Comment(id=1,comment='Test comment',user=self.user_Michael,pitch=self.new_pitch)
 
