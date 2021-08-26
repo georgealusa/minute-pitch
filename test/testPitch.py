3384dbe28e9ff337b3716bf7990397b7f0fc7417
George Alusa <40027452+georgealusa@users.noreply.github.com>
2021-08-26 13:39:33 +0300

    Update testPitch.py

--- test/testPitch.py
+++ test/testPitch.py
@@ -4,7 +4,7 @@ import unittest
 
 class PitchModelTest(unittest.TestCase):
     def setUp(self):
-        self.user_Michael = User(username = 'Michael',password = 'Calman', email = 'michael.m.njoroge254@gmail.com')
+        self.user_Michael = User(username = 'George',password = 'Calman', email = 'georgealusa@gmail.com')
         self.new_pitch = Pitch(id=1,pitch_title='Test',pitch_content='This is a test pitch',category="interview",user = self.user_Michael,likes=0,dislikes=0)
 
     def tearDown(self):
