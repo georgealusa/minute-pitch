a8a7b56d9f21c34f93d0df27be8b05afdcf3c069
georgealusa <georgealusa@gmail.com>
2021-08-26 13:08:57 +0300

    Initial Commit

---
+++ config.py
@@ -0,0 +1,53 @@
+import os
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = '1234!'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    
+     #email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'pitch'
+    SENDER_EMAIL = 'michael.m.njoroge254@gmail.com'
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    # TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Calmanb@localhost/pitch'
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Calman@localhost/pitch'
+    DEBUG = True
+    ENV = 'development'
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
---
+++ test/testComment.py
@@ -0,0 +1,18 @@
+from app.models import Comment,User,Pitch
+from app import db
+import unittest
+
+class CommentModelTest(unittest.TestCase):
+    def setUp(self):
+        self.user_Michael = User(username = 'Michael',password = 'banana', email = 'michael.m.njoroge254@gmail.com')
+        self.new_pitch = Pitch(id=1,pitch_title='Test',pitch_comment='This is a test pitch',category="interview",user = self.user_Dorcas,likes=0,dislikes=0)
+        self.new_comment = Comment(id=1,comment='Test comment',user=self.user_Michael,pitch=self.new_pitch)
+
+    def tearDown(self):
+        Pitch.query.delete()
+        User.query.delete()
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_comment.comment,'Test comment')
+        self.assertEquals(self.new_comment.user,self.user_Michael)
+        self.assertEquals(self.new_comment.pitch,self.new_pitch)
